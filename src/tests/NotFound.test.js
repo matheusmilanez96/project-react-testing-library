2ad20d090393e7f6689e695918808e297634a6a3
Matheus Milanez <mattmila96@gmail.com>
2023-02-27 23:03:46 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+const notFoundText = 'Page requested not found';
+
+describe('Testa o componente NotFound', () => {
+  it('Testa se a página contém um heading com o texto necessário', () => {
+    render(<NotFound />);
+
+    const notFoundEl = screen.getByRole(
+      'heading',
+      { name: notFoundText },
+    );
+
+    expect(notFoundEl).toBeInTheDocument();
+  });
+
+  it('Testa se a página mostra a imagem necessária', () => {
+    render(<NotFound />);
+
+    const imgSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    const img = screen.getByAltText('Pikachu crying because the page requested was not found');
+
+    expect(img).toHaveAttribute('src', imgSrc);
+  });
+});
